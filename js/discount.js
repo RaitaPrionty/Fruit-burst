c6c5ca13ac3b5557c8d09dfeb7a86f404acb05a5
raitaislam23 <priontyraitaislam@gmail.com>
2024-02-09 02:27:40 +0600

    this is my commit for calculation discount

--- js/discount.js
+++ js/discount.js
@@ -13,71 +13,107 @@ function fruitsPrice(id){
 document.getElementById('btn-mango').addEventListener('click',function(){
     
     const price = fruitsPrice('Mango-price');
-    const input = document.getElementById('Mango-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('mango-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('Mango-input');
+    input.value = discountPrice;
 })
 // jackfruit
 document.getElementById('btn-jackfruit').addEventListener('click',function(){
     
     const price = fruitsPrice('jackfruit-price');
-    const input = document.getElementById('jackfruit-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('jackfruit-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('jackfruit-input');
+    input.value = discountPrice;
 })
 // watermelon
 document.getElementById('btn-watermelon').addEventListener('click',function(){
     
     const price = fruitsPrice('watermelon-price');
-    const input = document.getElementById('watermelon-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('watermelon-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('watermelon-input');
+    input.value = discountPrice;
 })
 // balckberry
 document.getElementById('berry-btn').addEventListener('click',function(){
     
     const price = fruitsPrice('berry-price');
-    const input = document.getElementById('berry-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('blackberry-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('berry-input');
+    input.value = discountPrice;
 })
 // strawberry
 document.getElementById('straw-btn').addEventListener('click',function(){
     
     const price = fruitsPrice('straw-price');
-    const input = document.getElementById('straw-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('strawberry-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('straw-input');
+    input.value = discountPrice;
 })
 // waterfruits
 document.getElementById('btn-waterfruit').addEventListener('click',function(){
     
     const price = fruitsPrice('waterfruit-price');
-    const input = document.getElementById('waterfruit-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('waterfruit-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('waterfruit-input');
+    input.value = discountPrice;
 })
 // waterapple
 document.getElementById('apple-btn').addEventListener('click',function(){
     
     const price = fruitsPrice('apple-price');
-    const input = document.getElementById('apple-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('waterapple-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('apple-input');
+    input.value = discountPrice;
 })
 // plam
 document.getElementById('palm-btn').addEventListener('click',function(){
     
     const price = fruitsPrice('palm-price');
-    const input = document.getElementById('palm-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('palm-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('palm-input');
+    input.value = discountPrice;
 })
 // litchi
 document.getElementById('btn-litchi').addEventListener('click',function(){
     
     const price = fruitsPrice('litchi-price');
-    const input = document.getElementById('litchi-input');
-    const discount = price * 20/100;
-    input.value = discount;
+    const mangoquantity = document.getElementById('litchi-quantity');
+    const mangoquantitystring = mangoquantity.value;
+    const mangoquantityfloat = parseFloat(mangoquantitystring);
+    const PriceMango = mangoquantityfloat*price
+    const discountPrice = PriceMango * 20/100;
+   const input = document.getElementById('litchi-input');
+    input.value = discountPrice;
 })
